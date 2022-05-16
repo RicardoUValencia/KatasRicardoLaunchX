63003345e4016d42468b2c96d78f007ae10476e6
RicardoUValencia <87098001+RicardoUValencia@users.noreply.github.com>
2022-05-16 08:12:34 -0500

    add: Modulo 3

---
+++ Modulo 3/asteroidev3.py
@@ -0,0 +1,8 @@
+vel_asteroide = 49
+longitud = 32
+if vel_asteroide > 20 and longitud > 32: 
+    print("Una luz se ve mas cerca de la tierra")
+elif vel_asteroide >= 20: 
+    print("Hay una luz pareciera ser un asteroide")
+else:
+    print("Todos estamos a salvo")
